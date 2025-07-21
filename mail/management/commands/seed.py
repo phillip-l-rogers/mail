7d6de971272d83fa93023a8f0b2e3ade03397caa
Phillip Rogers <phillip.l.rogers.29@gmail.com>
2025-07-21 13:44:39 -0700

    Fixed seed code

--- mail/management/commands/seed.py
+++ mail/management/commands/seed.py
@@ -26,22 +26,27 @@ class Command(BaseCommand):
         alice = User.objects.get(username="alice@example.com")
         bob = User.objects.get(username="bob@example.com")
         charlie = User.objects.get(username="charlie@example.com")
-        self.stdout.write(
-            self.style.SUCCESS("✅ Users created")
-        )  # Create a dummy email
-        Email.objects.create(
-            user=charlie,
-            sender=alice,
-            subject="Welcome to Mail App!",
-            body="This is a test email sent from Alice to Test User.",
-            timestamp="2025-07-17 15:00",
-        ).recipients.add(charlie)
+        self.stdout.write(self.style.SUCCESS("✅ Users created"))
         # Create a dummy email
-        Email.objects.create(
-            user=charlie,
-            sender=bob,
-            subject="Welcome to Mail App!",
-            body="This is a test email sent from Bob to Test User.",
-            timestamp="2025-07-18 15:00",
-        ).recipients.add(charlie)
+        for user in (alice, charlie):
+            email1 = Email.objects.create(
+                user=user,
+                sender=alice,
+                subject="Welcome to Mail App!",
+                body="This is a test email sent from Alice to Charlie.",
+                timestamp="2025-07-17 15:00",
+            )
+            email1.recipients.add(charlie)
+            email1.save()
+        # Create another dummy email
+        for user in (bob, charlie):
+            email2 = Email.objects.create(
+                user=user,
+                sender=bob,
+                subject="Welcome to Mail App!",
+                body="This is a test email sent from Bob to Charlie.",
+                timestamp="2025-07-18 15:00",
+            )
+            email2.recipients.add(charlie)
+            email2.save()
         self.stdout.write(self.style.SUCCESS("Dummy data seeded."))
